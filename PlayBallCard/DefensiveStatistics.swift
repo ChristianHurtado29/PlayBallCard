466a2d74d8da2104f65eb1b0dcbf6d538c028311
Christian Hurtado <christian.hurtado.catacoli@gmail.com>
2020-10-19 22:28:58 -0400

    Initial Commit

---
+++ PlayBallCard/DefensiveStatistics.swift
@@ -0,0 +1,15 @@
+//
+//  DefensiveStatistics.swift
+//  PlayBallCard
+//
+//  Created by Christian Hurtado on 10/19/20.
+//
+
+import Foundation
+
+struct DefensiveStatistics {
+    let assits: Int
+    let putouts: Int
+    let errors: Int
+    let fieldingPct: Double
+}
---
+++ PlayBallCard/OffensiveStatistics.swift
@@ -0,0 +1,23 @@
+//
+//  OffensiveStatistics.swift
+//  PlayBallCard
+//
+//  Created by Christian Hurtado on 10/19/20.
+//
+
+import Foundation
+
+struct OffensiveStatistics {
+    let hits: Int
+    let strikeouts: Int
+    let walks: Int
+    let battingAverage: Double
+    let ops: Double
+    let sacHits: Int
+    let runsBattedIn: Int
+    let homeruns: Int
+    let doubles: Int
+    let triples: Int
+    let totalBases: Int
+    let stolenBases: Int
+}
